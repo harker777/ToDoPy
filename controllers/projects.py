c7482619d83f9e03b1c16b011671620edd5377d3
Iaroslav Mazai <iaroslav.mazai@travisperkins.co.uk>
2015-07-01 01:04:29 +0300

    Added project details page

--- controllers/projects.py
+++ controllers/projects.py
@@ -1,6 +1,12 @@
 from applications.todo.models.dao.userdao import UserDao
+from applications.todo.models.dao.taskdao import TaskDao
 from gluon import current
 
 def index():
     rows = UserDao.getProjectsForUser(current.userId)
     return dict(projects = rows)
+
+def details():
+	projectId = request.args[0]
+	tasks = TaskDao.getTasksForProject(projectId)
+	return dict(tasks = tasks)
--- models/dao/taskdao.py
+++ models/dao/taskdao.py
@@ -3,5 +3,10 @@ from gluon import current
 class TaskDao:
     @staticmethod
     def getAllTasks():
-        db = current.db;
-        return db().select(db.task.ALL);
+        db = current.db
+        return db().select(db.task.ALL)
+
+    @staticmethod
+    def getTasksForProject(projectId):
+        db = current.db
+        return db(db.task.project_id == projectId).select(db.task.ALL)
